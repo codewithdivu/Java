9c72ecfa2440d90d90aa7ad8dd95f484351bbc02
codewithdivu <mavadiyadivyesh56@gmail.com>
2024-08-09 11:59:52 +0530

    Initial Commit

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello {
+    public static void main(String[] args) {
+        int num = 10;
+        String name = "Divyesh";
+        boolean isMarried = false;
+        float height = 182.5f;
+        char myLetter = 'D';
+        System.out.println(Math.max(num, height));
+    }
+}
